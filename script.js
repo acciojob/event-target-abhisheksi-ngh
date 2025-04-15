d6bc9a58cd0d4f57ab068d6dbdf09bd69fbffe65
A <abhishekkumarsingh9585@gmail.com>
2025-04-15 23:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map(); // Proper Map initialization
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
+    }
+
+    const callbacks = this.listeners.get(event);
+
+    if (!callbacks.includes(callback)) {
+      callbacks.push(callback);
+    }
+  }
+
+  removeEventListener(event, callback) {
+    if (!this.listeners.has(event)) return;
+
+    const callbacks = this.listeners.get(event);
+    const filtered = callbacks.filter(cb => cb !== callback);
+    this.listeners.set(event, filtered);
+  }
+
+  dispatchEvent(event) {
+    if (!this.listeners.has(event)) return;
+
+    const callbacks = this.listeners.get(event);
+    for (const cb of callbacks) {
+      cb();
+    }
+  }
+}
